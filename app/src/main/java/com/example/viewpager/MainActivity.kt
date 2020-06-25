4025c398f1b0ab639fe486385f9b9b150b5fe811
t-kento <bluestarsplace@yahoo.co.jp>
2020-06-25 20:13:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val images = listOf(
+            R.drawable.photo1,
+            R.drawable.photo2,
+            R.drawable.photo3,
+            R.drawable.photo4,
+            R.drawable.photo5,
+            R.drawable.photo6,
+            R.drawable.photo7
+        )
+
+        val adapter = ViewPagerAdapter(images)
+        viewPager.adapter = adapter
+
+        viewPager.orientation=ViewPager2.ORIENTATION_HORIZONTAL
+
+    }
+}
---
+++ app/src/main/java/com/example/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.viewpager
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_view_pager.view.*
+
+class ViewPagerAdapter (
+    val images:List<Int>
+):RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>(){
+    inner class ViewPagerViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+       val view =LayoutInflater.from(parent.context).inflate(R.layout.item_view_pager,parent,false)
+        return ViewPagerViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = images.size
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+       val curImage = images[position]
+        holder.itemView.ivImage.setImageResource(curImage)
+    }
+
+}
